4259a2aeb7f72d0d665692f16852b64ca15b8995
seungwon0113 <jungseungwon0113@gmail.com>
2025-09-26 07:59:48 +0900

    mypage

---
+++ users/views/mypage.py
@@ -0,0 +1,52 @@
+from typing import cast
+
+from django.contrib.auth.decorators import login_required
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic.base import View
+
+from users.models import User
+
+
+@method_decorator(login_required, name='dispatch')
+class MyPageView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        user = cast(User, request.user)
+        
+        #TODO: 주문 현황 데이터 (추후 Order 모델이 생기면 실제 데이터로 대체)
+        order_stats = {
+            'pending': 0,
+            'preparing': 0,
+            'shipping': 0,
+            'delivered': 0,
+        }
+        
+        #TODO: 소핑백 현황 (추후 Cart 모델이 생기면 실제 데이터로 대체)
+        cart_stats = {
+            'items': 0,
+            'likes': 0,
+            'reviews': 0,
+        }
+        
+        context = {
+            'user': user,
+            'order_stats': order_stats,
+            'cart_stats': cart_stats,
+            'current_page': 'order_mypage',
+        }
+        
+        return render(request, "orders/order_mypage.html", context)
+    
+@method_decorator(login_required, name='dispatch')
+class ProfileEditView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        user = cast(User, request.user)
+        
+        context = {
+            'user': user,
+            'current_page': 'profile_edit'
+        }
+        
+        return render(request, "users/profile_edit.html", context)
