2898e08f5c3837375ecbee6d47f45454061e587c
seungwon0113 <jungseungwon0113@gmail.com>
2025-10-02 17:44:37 +0900

    conneting category

---
+++ categories/context_processors.py
@@ -0,0 +1,10 @@
+from typing import Dict, Any
+from django.http import HttpRequest
+from categories.models import Category
+
+
+def categories_context(request: HttpRequest) -> Dict[str, Any]:
+    main_categories = Category.objects.filter(parent=None).prefetch_related('children')[:4]
+    return {
+        'main_categories': main_categories
+    }
